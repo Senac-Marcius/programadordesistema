af5682330c7a5e82aefcc524ec47d72fbd415dd4
MarciusMcflay <jhorge.marcius@gmail.com>
2025-08-11 21:09:30 -0300

    calculadora feita

--- pi_react/src/App.js
+++ pi_react/src/App.js
@@ -2,22 +2,25 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() { //aqui é java script
- let oi = "Ola mundo";
 
-  oi += " Marcius";
-
-  function soma(a, b){ // parametros 
-    return a + b;
-  }
-
-  function divide(a, b){
-    return a /b;
+  function calculadora (a, b, op){
+    if(op == '+'){
+      return a + b
+    } else if(op == '-'){
+      return a - b;
+    } else if (op == '*') {
+      return a * b
+    } else if (op == '/') {
+      return a / b
+    }
   }
 
   return ( /* aqui é html */
     <main className="App">
-      {soma(oi, " Leandro")}<br/>
-      {divide(36, 6)}
+      { calculadora(5,  calculadora(8, 7, '*'), '+') }<br/>
+      { calculadora(5, 5, '-') }<br/>
+      { calculadora(5, 5, '*') }<br/>
+      { calculadora(5, 5, '/') }<br/>
     </main>
   );
 }
