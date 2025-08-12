8f7ddeea068c84bffe295c650a82f6979d9bba3a
MarciusMcflay <jhorge.marcius@gmail.com>
2025-08-12 19:01:08 -0300

    exercicio a ser feito

--- pi_react/src/App.js
+++ pi_react/src/App.js
@@ -2,20 +2,12 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() { //aqui é java script
-
-  let cesta = ['pão', 'suco', 'chocolate', 'torrone']
-
-  let desenha = [];
-  // declara o indice; compara se é pra continuar; incrementa o indice
-  for(let i=0; i<3; i++){
-    desenha.push(<p> { cesta[i]  } </p>)
-  }
+  //Faça uma lista com todos os numeros até 10
+  // mas exiba apenas o números impares
 
   return ( /* aqui é html */
     <main className="App">
-      {
-        desenha
-      }
+     
     </main>
   );
 }
