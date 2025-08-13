7caf8005fa509615edb340090091f192a48a360d
MarciusMcflay <jhorge.marcius@gmail.com>
2025-08-12 22:08:51 -0300

    esta com o as duas telas

--- pi_react/src/App.js
+++ pi_react/src/App.js
@@ -2,21 +2,72 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() { //aqui é java script
-  //Faça uma lista com todos os numeros até 10
-  const numbers = [4,1,2,3,5,6,7,8,9,10];
-  
-  // mas exiba apenas o números impares
-  let impars = [];
+  //quero uma variavel para o email
+  let email = ""
 
-  for(let i=0; i< numbers.length; i++){
-    if( numbers[i] % 2 != 0){ //if( numbers[i] % 2 == 1){
-      impars.push(<p> { numbers[i] } </p>);
-    }
+  function mudaEmail(valor){
+    email = valor
+  }  
+
+  //quero uma variavel para a senha
+  let senha = ""
+
+  function mudaSenha(valor){
+    senha = valor
+  }
+
+  function enviar(){
+    alert("Email:"+email+" senha: "+senha)
+  }
+
+  let isLogin = true;
+
+  function mudaTela(){
+    isLogin = !isLogin
   }
 
   return ( /* aqui é html */
     <main className="App">
-      {impars}
+      <button onClick={ () => mudaTela()}>
+        {isLogin && ("Cadastrar-se")}
+        {!isLogin && ("Voltar para o login")}
+      </button>
+      
+      {!isLogin && (
+      <form className="register">
+        
+      </form>
+      )}
+
+      {isLogin && (
+      <form className="login">
+        <label>
+          Email: 
+          <input 
+            type="Email" 
+            name="email" 
+            placeholder="email@exemplo.com" 
+            onChange={ (e) => mudaEmail(e.target.value) }
+          /><br/>
+        </label>
+        <label>
+          Senha: 
+          <input 
+            type="password" 
+            name="Senha" 
+            placeholder="coloque uma senha com 6 carcater"
+            onChange={ (e) => mudaSenha(e.target.value) }
+          /><br/>
+        </label>
+        <button 
+          className="buttonSucess"
+          onClick={() => enviar()}
+        >
+          Login
+        </button>
+      </form>
+      )}
+
     </main>
   );
 }
